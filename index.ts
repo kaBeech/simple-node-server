488f63e74f2665684bb1304718161cd116e09d8d
QSL <kbeechly@icloud.com>
2022-11-15 19:05:03 -0800

    Dry code

--- index.ts
+++ index.ts
@@ -5,23 +5,22 @@ http.createServer(function (req, res) {
 
     const fileName = "." + req.url;
     
-    const fourOhFour = () => fs.readFile('./404.html', function(err, data) {
+    const fourOhFour = () => fs.readFileSync('./404.html', function(err, data) {
         if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            res.write("404 Page Not Found");
-            return res.end();
+            return '404 Page Not Found';
         }
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
+        return data;
     });
 
     fs.readFile(fileName, function(err, data) {
         if (err) {
-            return fourOhFour();
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.write(fourOhFour());
+            return res.end();
         }
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data)
         return res.end();
     })
+    
 }).listen(8080);
