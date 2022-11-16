b6e34d01f31d5fc708d999aeb6c2dcb9e48dd750
QSL <kbeechly@icloud.com>
2022-11-15 16:57:11 -0800

    Implement 404 page with url.parse

--- index.ts
+++ index.ts
@@ -7,11 +7,19 @@ http.createServer(function (req, res) {
     const reqURL = url.parse(req.url, true);
     // const reqURL = new URL(req.url);  
     const fileName = "." + reqURL.pathname;
-    fs.readFile(fileName, function(err, data) {
+    const fourOhFour = () => fs.readFile('./404.html', function(err, data) {
         if (err) {
             res.writeHead(404, {'Content-Type': 'text/html'});
-            res.write("forOhFor")
+            res.write("404 Page Not Found");
             return res.end();
+        }
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+    fs.readFile(fileName, function(err, data) {
+        if (err) {
+            return fourOhFour();
         }
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data)
